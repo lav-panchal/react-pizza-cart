4cd300cd2f31518287ad07235ee51f8dd9a8a1c0
Lav <lovepanchal1912@gmail.com>
2021-09-08 00:02:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import Navigation from "./Components/Navigation";
+import ProductsPage from "./Pages/ProductsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navigation />
+
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/products" component={ProductsPage}></Route>
+          <Route path="/cart" component={Cart}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Pages/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Products from "../Components/Products";
+const ProductsPage = () => {
+  return (
+    <>
+      <Products />
+    </>
+  );
+};
+
+export default ProductsPage;
